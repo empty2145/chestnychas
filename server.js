355420e8c794e814e7704aae064bac579b8800fd
empty2145 <tasybaevtamir@gmail.com>
2026-09-20 22:50:43 +0500

    add: cors to bypass the security block

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const HonestHour = require('./models/HonestHour');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI)
